a5aa2a7714695bd7e0e28631546fb5f3fb92ab82
Chanaka Sampath <ChanakaS@millenniumitesp.com>
2019-08-26 13:48:38 +0530

    Updates

---
+++ precache-manifest.4464c1501b61fe8a524990a5aaf9be3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6fc94e722039383b701d68c308d3477",
+    "url": "/daily-news-with-react-redux/index.html"
+  },
+  {
+    "revision": "786626184d2d5d318518",
+    "url": "/daily-news-with-react-redux/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "a57a6a0bad3bca3c51ff",
+    "url": "/daily-news-with-react-redux/static/js/2.35da353c.chunk.js"
+  },
+  {
+    "revision": "786626184d2d5d318518",
+    "url": "/daily-news-with-react-redux/static/js/main.71d89258.chunk.js"
+  },
+  {
+    "revision": "f82635083ab9d9522ddf",
+    "url": "/daily-news-with-react-redux/static/js/runtime~main.5b27b7c0.js"
+  }
+]);
